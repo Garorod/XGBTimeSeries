b7f99083156ce7a488ab293d93baafa28832a4d0
yawerijaz-mfe <yawer.ijaz@berkeley.edu>
2021-01-31 10:01:25 -0800

    data load funcs

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,5 @@
 from glob import glob
 
 from src.data.constants import *
-
+from src.data.load import debug_buckets, train_buckets, valid_buckets, test_buckets
 parquet_paths = glob(parquet_dir + '*')
