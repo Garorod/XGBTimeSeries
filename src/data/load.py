9667e591c0e89d5f0319d2499a4efc4864526476
yawerijaz-mfe <yawer.ijaz@berkeley.edu>
2021-01-31 02:40:41 -0800

    oversample

--- src/data/load.py
+++ src/data/load.py
@@ -5,15 +5,21 @@ import os.path
 from joblib import Parallel, delayed
 from tqdm import tqdm
 
+from imblearn.over_sampling import RandomOverSampler
+
 from src.data import trimmed_data_dir, x_cols, y_cols
 
 
-def make_bucket_sep_index(bucket, return_index=False):
+def make_bucket_sep_index(bucket, return_index=False, oversample=True):
     bucket_paths = glob(os.path.join(trimmed_data_dir, f'*.bucket_{bucket:02}'))
     bucket_data = pd.concat(pd.read_parquet(p) for p in bucket_paths)
     # loan id number too big for joblib
     val = bucket_data.reset_index(drop=True)
     X, y = val.reindex(x_cols, axis=1), val.reindex(y_cols, axis=1)
+    if oversample:
+        X, y = RandomOverSampler().fit_resample(X.values, y.values)
+        X = pd.DataFrame(X, columns=x_cols)
+        y = pd.DataFrame(y, columns=y_cols)
     if return_index:
         idx = bucket_data.index.to_frame().reset_index(drop=True).astype(str)
         return X, y, idx
@@ -22,8 +28,8 @@ def make_bucket_sep_index(bucket, return_index=False):
 
 
 # %%
-def make_buckets(buckets, return_index=False):
-    xys = Parallel(n_jobs=8)(delayed(make_bucket_sep_index)(buc, return_index) for buc in tqdm(buckets))
+def make_buckets(buckets, return_index=False, oversample=True):
+    xys = Parallel(n_jobs=8)(delayed(make_bucket_sep_index)(buc, return_index, oversample) for buc in tqdm(buckets))
     idxs, Xs, ys = [], [], []
     for xy in xys:
         if return_index:
